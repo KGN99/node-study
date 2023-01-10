189795f6d5d5075f9be6f95ce29b4c0eb60899e8
kgn99 <y3ef9484@gmail.com>
2023-01-10 14:26:27 +0900

    핸드폰 인증 토큰 발급 API / FACADE 패턴으로 리팩토링

---
+++ node-js/01-token-api-facade/index.js
@@ -0,0 +1,48 @@
+function checkValidationPhone(myphone) {
+    // 휴대폰 번호 자릿수 맞는지 확인
+    if (myphone.length !== 10 && myphone.length !== 11) {
+        // 에러 메시지 콘솔에 출력
+        console.log("Error : 핸드폰 번호를 확인해주세요.");
+        return false;
+    }
+    return true;
+}
+
+function getToken(count) {
+    // 핸드폰 토큰 6자리 만들기
+    if (count === undefined) {
+        console.log("에러 발생!!! 갯수를 제대로 입력해 주세요!!!");
+        return;
+    } else if (count <= 0) {
+        console.log("에러 발생!!! 갯수가 너무 적습니다!!!");
+        return;
+    } else if (count > 10) {
+        console.log("에러 발생!!! 갯수가 너무 많습니다!!!");
+        return;
+    }
+    const result = String(Math.floor(Math.random() * 10 ** count)).padStart(
+        count,
+        "0"
+    );
+    return result;
+}
+
+function sendTokenToSMS(myphone, token) {
+    // 핸드폰 번호에 토큰 전송하기
+    console.log(myphone + " 번호로 인증번호 " + token + " 를 전송합니다.");
+}
+function createTokenOfPhone(myphone, count) {
+    // 1. 휴대폰번호 자릿수 맞는지 확인하기
+    const isValid = checkValidationPhone(myphone);
+
+    if (isValid === true) {
+        // 2. 핸드폰 토큰 6자리 만들기
+        const token = getToken(count);
+
+        // 3. 핸드폰번호에 토큰 전송하기
+        sendTokenToSMS(myphone, token);
+    }
+}
+
+// API 실행하기
+createTokenOfPhone("01012345678", 6);
