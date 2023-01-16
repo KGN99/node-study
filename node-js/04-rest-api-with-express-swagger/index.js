503328b99370d44b5b2f9fe1fc2e651557352945
kgn99 <y3ef9484@gmail.com>
2023-01-16 17:36:19 +0900

    swagger API 명세서

---
+++ node-js/04-rest-api-with-express-swagger/index.js
@@ -0,0 +1,69 @@
+import express from "express";
+import { checkValidationPhone, getToken, sendTokenToSMS } from "./phone.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+import { options } from "./swagger/config.js";
+
+const app = express();
+const port = 3000;
+
+const swaggerSpec = swaggerJsdoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// req.body 읽기 위해 선언
+app.use(express.json());
+
+app.get("/boards", (req, res) => {
+    // 1. 데이터를 조회
+    const result = [
+        {
+            number: 1,
+            writer: "철수",
+            title: "제목입니다.",
+            contents: "철수 내용",
+        },
+        {
+            number: 2,
+            writer: "영희",
+            title: "제목입니다.",
+            contents: "영희 내용",
+        },
+        {
+            number: 3,
+            writer: "훈이",
+            title: "제목입니다.",
+            contents: "훈이 내용",
+        },
+    ];
+    // 2. 결과 응답
+    res.send(result);
+});
+
+app.post("/boards", (req, res) => {
+    console.log(req.body);
+    // 1. 데이터를 등록
+    // 2. 저장 결과 응답
+    res.send("게시물 등록에 성공하였습니다.");
+});
+
+app.post("/tokens/phone", (req, res) => {
+    function createTokenOfPhone(myphone, count) {
+        // 1. 휴대폰번호 자릿수 맞는지 확인하기
+        const isValid = checkValidationPhone(myphone);
+
+        if (isValid === true) {
+            // 2. 핸드폰 토큰 6자리 만들기
+            const token = getToken(count);
+
+            // 3. 핸드폰번호에 토큰 전송하기
+            const result = sendTokenToSMS(myphone, token);
+            console.log(result);
+            res.send(result);
+        }
+    }
+    createTokenOfPhone(req.body.phone, 6);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ node-js/04-rest-api-with-express-swagger/phone.js
@@ -0,0 +1,33 @@
+export function checkValidationPhone(myphone) {
+    // 휴대폰 번호 자릿수 맞는지 확인
+    if (myphone.length !== 10 && myphone.length !== 11) {
+        // 에러 메시지 콘솔에 출력
+        console.log("Error : 핸드폰 번호를 확인해주세요.");
+        return false;
+    }
+    return true;
+}
+
+export function getToken(count) {
+    // 핸드폰 토큰 6자리 만들기
+    if (count === undefined) {
+        console.log("에러 발생!!! 갯수를 제대로 입력해 주세요!!!");
+        return;
+    } else if (count <= 0) {
+        console.log("에러 발생!!! 갯수가 너무 적습니다!!!");
+        return;
+    } else if (count > 10) {
+        console.log("에러 발생!!! 갯수가 너무 많습니다!!!");
+        return;
+    }
+    const result = String(Math.floor(Math.random() * 10 ** count)).padStart(
+        count,
+        "0"
+    );
+    return result;
+}
+
+export function sendTokenToSMS(myphone, token) {
+    // 핸드폰 번호에 토큰 전송하기
+    return myphone + " 번호로 인증번호 " + token + " 를 전송합니다.";
+}
